3571808fbf3234b7648049c9096356ae5b25fb6e
Paul Elisha <ireajao@yahoo.com>
2023-09-11 19:47:27 +0100

    first commit

---
+++ contracts/interface/INFT.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.19;
+
+interface INFT {
+    function safeMint(uint256 tokenId) external;
+}
---
+++ scripts/nft.ts
@@ -0,0 +1,28 @@
+import { ethers, network } from "hardhat";
+
+const main = async () => {
+  const nft = await ethers.deployContract("ZEUS");
+
+  await nft.waitForDeployment();
+  console.log(nft.target);
+
+  // const user = "0x24aa0E53a279D64AD7aaf49F7c420F8193967502";
+
+  // const userSigner = await ethers.getImpersonatedSigner(user);
+
+  // const interact = await ethers.getContractAt(
+  //   "INFT",
+  //   "0x9364870D1cEcDffa5b493363ab62002053748B0D"
+  // );
+
+  // const nftCID = "QmWFADeXjSYis3UsDRsKh4b5FLvrfkRZtT89Yu3ZYFCdCV";
+
+  // const play = await interact.connect(userSigner).safeMint(nftCID);
+
+  // console.log(await ethers.formatUnits(String(play)));
+};
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
